ee22e2fa912eaa41116c1f1b39ab4f41c6b8efc5
Priyanka sharma <76725172+Priyankasharma181@users.noreply.github.com>
2021-11-20 18:11:19 +0530

    Add files via upload

---
+++ lastnum.py
@@ -0,0 +1,15 @@
+num = int(input("enter the num :"))
+a = []
+i=0
+while i<num:
+    number = int(input("enter the number:"))
+    a.append(number)
+    i+=1
+# print(a)
+b = []    
+j = 0
+while j<len(a):
+    if a[2]== 2:
+        b.append(a)
+    j+=1 
+print(b)             
